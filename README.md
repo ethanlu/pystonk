178426a83ffe8b30f0e520906638d4c7b8a4ba6b
Ethan Lu <fang.lu@gmail.com>
2022-04-12 01:13:55 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# pystonk
-python tool to help in trading stocks and options
+pystonk
+=======
+
+Python tool that leverages publically available stock data to do some simple calculations that I often perform when
+trading stocks and options.
+
+Uses [TD Amertrade's API](https://developer.tdameritrade.com/apis) data to generate stock and option reports.
+
+# Table of Contents
+* [Install/Run](#install-run)
+* [TODO](#todo)
+
+## Install/Run
+
+## TODO
+- Docker
+- Tests
+- Kivy desktop app
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name="pytstonk",
+    version="1.0.0",
+    author="Ethan Lu",
+    author_email="fang.lu@gmail.com",
+    description="Python Stocks & Options Tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ethanlu/pystonk",
+    license="MIT",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    python_requires='>3.6',
+    packages=find_packages(),
+    package_data={'pyanoled': ['conf/*.conf']},
+    install_requires=[
+        "argparse",
+        "numpy",
+        "pyhocon",
+        "requests",
+        "wheel"
+    ],
+    entry_points={
+        'console_scripts': [
+            'pystonk = pystonk.__main__:main'
+        ]
+    }
+)
