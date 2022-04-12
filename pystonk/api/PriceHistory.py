3ceaf80b70812844cb7be914b48b09805688b20b
Ethan Lu <fang.lu@gmail.com>
2022-04-12 01:52:51 -0400

    price history api

---
+++ pystonk/api/PriceHistory.py
@@ -0,0 +1,66 @@
+from pystonk.api.types.PeriodType import PeriodType
+from pystonk.api.types.FrequencyType import FrequencyType
+from pystonk.common.LoggerMixin import LoggerMixin
+from pystonk.common.models.CandleStick import CandleStick
+
+from datetime import datetime
+from typing import Any, Dict, List
+
+import requests
+
+
+class PriceHistory(LoggerMixin):
+    ENDPOINT = "https://api.tdameritrade.com/v1/marketdata/{symbol}/pricehistory"
+
+    def __init__(self, api_key: str):
+        self._api_key = api_key
+        self.logger.info(f"initialized with api key : {api_key}")
+
+    def _buildResponse(self, candlesticks: List[Dict[str, Any]], frequency_type: FrequencyType) -> List[CandleStick]:
+        return [CandleStick(data['open'], data['high'], data['low'], data['close'], data['volume'], data['datetime'], frequency_type) for data in candlesticks]
+
+    def getPriceHistory(self, symbol: str,
+            period_type: PeriodType = PeriodType.YTD,
+            period: int = 1,
+            frequency_type: FrequencyType = FrequencyType.DAILY,
+            frequency = 1) -> List[CandleStick]:
+        symbol = symbol.upper()
+        params = {
+            'apikey': self._api_key,
+            'periodType': period_type.value,
+            'period': period,
+            'frequencyType': frequency_type.value,
+            'frequency': frequency,
+            'needExtendedHoursData': False
+        }
+        self.logger.debug(f"getting price history for {symbol} with params : {params}")
+        response = requests.get(
+            self.ENDPOINT.format(symbol=symbol),
+            params=params
+        )
+        self.logger.debug(f"request : {response.url}")
+        self.logger.debug(f"response : {response}")
+        return self._buildResponse(response.json()['candles'], frequency_type)
+
+    def getPriceHistoryWithDateRange(self, symbol: str,
+            start: datetime,
+            end: datetime,
+            period_type: PeriodType = PeriodType.YTD,
+            frequency_type: FrequencyType = FrequencyType.DAILY,
+            frequency = 1) -> List[CandleStick]:
+        symbol = symbol.upper()
+        params = {
+            'apikey': self._api_key,
+            'periodType': period_type.value,
+            'frequencyType': frequency_type.value,
+            'frequency': frequency,
+            'startDate': start.timestamp() * 1000,
+            'endDate': end.timestamp() * 1000,
+            'needExtendedHoursData': False
+        }
+        self.logger.debug(f"getting date ranged price history for {symbol} with params : {params}")
+        response = requests.get(self.ENDPOINT.format(symbol=symbol), params=params)
+        self.logger.debug(f"request : {response.url}")
+        self.logger.debug(f"response : {response}")
+        return self._buildResponse(response.json()['candles'], frequency_type)
+
