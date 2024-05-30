6cf18cfbcf556cd870edb740448cef69dba8ac97
Ethan Lu <fang.lu@gmail.com>
2024-05-29 22:54:02 -0400

    updated PriceHistoryApi to work with schwab api

--- pystonk/api/PriceHistoryApi.py
+++ pystonk/api/PriceHistoryApi.py
@@ -2,60 +2,37 @@ from pystonk.api import Api
 from pystonk.api.Types import PeriodType, FrequencyType
 from pystonk.models.CandleStick import CandleStick
 
-from datetime import datetime
 from typing import Any, Dict, List
 
-import requests
+
+def build_response(candlesticks: List[Dict[str, Any]], frequency_type: FrequencyType) -> List[CandleStick]:
+    return [CandleStick(data['open'], data['high'], data['low'], data['close'], data['volume'], data['datetime'], frequency_type) for data in candlesticks]
 
 
 class PriceHistoryApi(Api):
-    ENDPOINT = "https://api.tdameritrade.com/v1/marketdata/{symbol}/pricehistory"
+    ENDPOINT = "https://api.schwabapi.com/marketdata/v1/pricehistory"
 
-    def __init__(self, api_key: str):
-        super().__init__(api_key)
-
-    def _build_response(self, candlesticks: List[Dict[str, Any]], frequency_type: FrequencyType) -> List[CandleStick]:
-        return [CandleStick(data['open'], data['high'], data['low'], data['close'], data['volume'], data['datetime'], frequency_type) for data in candlesticks]
+    def __init__(self, app_key: str, app_secret: str):
+        super().__init__(app_key, app_secret)
 
     def get_price_history(self, symbol: str,
-            period_type: PeriodType = PeriodType.YTD,
-            period: int = 1,
-            frequency_type: FrequencyType = FrequencyType.DAILY,
-            frequency: int = 1) -> List[CandleStick]:
-        params = {
-            'apikey': self._api_key,
-            'periodType': period_type.value,
-            'period': period,
-            'frequencyType': frequency_type.value,
-            'frequency': frequency,
-            'needExtendedHoursData': False
-        }
-        self.logger.debug(f"getting price history for {symbol} with params : {params}")
-        response = requests.get(
-            self.ENDPOINT.format(symbol=symbol),
-            params=params
+                          period_type: PeriodType = PeriodType.YTD,
+                          period: int = 1,
+                          frequency_type: FrequencyType = FrequencyType.DAILY,
+                          frequency: int = 1) -> List[CandleStick]:
+        symbol = symbol.upper()
+        self.logger.debug(f"getting price history for {symbol}")
+        data = self._get(
+            self.ENDPOINT,
+            params={
+                'symbol': symbol,
+                'periodType': period_type.value,
+                'period': period,
+                'frequencyType': frequency_type.value,
+                'frequency': frequency,
+                'needExtendedHoursData': False,
+                'needPreviousClose': False
+            },
+            headers={'Authorization': f"Bearer {self.get_access_token()}"}
         )
-        self.logger.debug(f"request : {response.url}")
-        self.logger.debug(f"response : {response.status_code}")
-        return self._build_response(response.json()['candles'], frequency_type)
-
-    def get_price_history_with_date_range(self, symbol: str,
-            start: datetime,
-            end: datetime,
-            period_type: PeriodType = PeriodType.YTD,
-            frequency_type: FrequencyType = FrequencyType.DAILY,
-            frequency: int = 1) -> List[CandleStick]:
-        params = {
-            'apikey': self._api_key,
-            'periodType': period_type.value,
-            'frequencyType': frequency_type.value,
-            'frequency': frequency,
-            'startDate': start.timestamp() * 1000,
-            'endDate': end.timestamp() * 1000,
-            'needExtendedHoursData': False
-        }
-        self.logger.debug(f"getting date ranged price history for {symbol} with params : {params}")
-        response = requests.get(self.ENDPOINT.format(symbol=symbol), params=params)
-        self.logger.debug(f"request : {response.url}")
-        self.logger.debug(f"response : {response.status_code}")
-        return self._build_response(response.json()['candles'], frequency_type)
+        return build_response(data['candles'], frequency_type)
