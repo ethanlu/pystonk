d4565d954f4db1846ae3682401f92ac35b79c19e
Ethan Lu <fang.lu@gmail.com>
2024-05-29 22:00:06 -0400

    updated base Api class to use client_id/secret as credentials. added common methods for handling get/post calls. added method for retrieving access token

--- pystonk/api/__init__.py
+++ pystonk/api/__init__.py
@@ -1,8 +1,47 @@
 from pystonk.utils.LoggerMixin import LoggerMixin
 
+import requests
+
 
 class Api(LoggerMixin):
-    def __init__(self, api_key: str):
-        if not api_key:
-            raise ValueError(f"Invalid API Key : {api_key}")
-        self._api_key = api_key
+    ACCESS_TOKEN_ENDPOINT = "https://api.schwabapi.com/v1/oauth/token"
+    ACCESS_TOKEN = None
+
+    def __init__(self, app_key: str, app_secret: str):
+        if not app_key:
+            raise ValueError(f"Invalid App Key : {app_key}")
+        self._app_key = app_key
+
+        if not app_secret:
+            raise ValueError(f"Invalid App Secret : {app_secret}")
+        self._app_secret = app_secret
+
+    def _get(self, endpoint: str, **kwargs) -> dict:
+        self.logger.debug(f"get endpoint: {endpoint}")
+        self.logger.debug(f"get details: {kwargs}")
+        response = requests.get(endpoint, **kwargs)
+        self.logger.debug(f"get response : {response.status_code}")
+        response.raise_for_status()
+        return response.json()
+
+    def _post(self, endpoint: str, **kwargs) -> dict:
+        self.logger.debug(f"post endpoint: {endpoint}")
+        self.logger.debug(f"post details: {kwargs}")
+        response = requests.post(endpoint, **kwargs)
+        self.logger.debug(f"post response : {response.status_code}")
+        response.raise_for_status()
+        return response.json()
+
+    def get_access_token(self) -> str:
+        if not self.__class__.ACCESS_TOKEN:
+            data = self._post(
+                self.ACCESS_TOKEN_ENDPOINT,
+                data={'grant_type': "client_credentials", 'scope': 'pystonk'},
+                auth=(self._app_key, self._app_secret)
+            )
+
+            if not data or 'access_token' not in data:
+                raise ValueError(f"access token retreival returned successfully, but data was unrecognized : {data}")
+
+            self.__class__.ACCESS_TOKEN = data['access_token']
+        return self.__class__.ACCESS_TOKEN
