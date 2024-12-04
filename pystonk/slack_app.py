5f510119689e74710fa179c5efb75776b2fcfa6d
Ethan Lu <fang.lu@gmail.com>
2024-12-04 00:57:19 -0500

    updated pystonk to run on aws lambda as 2 lambdas to avoid timeout errors and duplicate messages bug due to slack having a 3 second response time

--- pystonk/slack_app.py
+++ pystonk/slack_app.py
@@ -1,23 +1,21 @@
-from pystonk import configuration
+from pystonk import configuration, logger
 from pystonk.api.OptionsChainApi import OptionsChainApi
 from pystonk.api.PriceHistoryApi import PriceHistoryApi
 from pystonk.api.QuoteApi import QuoteApi
 from pystonk.commands.PriceCheckCommand import PriceCheckCommand
 from pystonk.commands.PriceHistoryCommand import PriceHistoryCommand
 from pystonk.commands.OptionsChainCommand import OptionsChainCommand
-from pystonk.utils.LoggerMixin import getLogger
+from pystonk.lambda_receiver import acknowledgements
 from pystonk.views import View
 from pystonk.views.HelpView import HelpView
 
 from random import choice
 from slack_bolt import App
-from slack_bolt.adapter.aws_lambda import SlackRequestHandler
 from slack_sdk import WebClient
 from typing import Type
 
 import re
 
-logger = getLogger('pystonk')
 
 apis = {
     'quote': QuoteApi(configuration['app_key'], configuration['app_secret']),
@@ -30,16 +28,6 @@ commands = (
     OptionsChainCommand(apis['quote'], apis['options_chain'])
 )
 
-acknowledgements = (
-    "Absolutely!", "Acknowledged HQ.", "Affirmative, sir!", "Affirmative.", "All right!", "As you will.", "Commencing!", "Commencing.", "Confirmed.",
-    "Delighted to, sir!", "Excellent!", "Excellent.", "Haha! At last!", "Honor guide me!", "I copy that.", "I dig.", "I hear that.", "I'm all over it.",
-    "I'm goin!", "I'm gone.", "I'm on the job.", "Immediately!", "In the pipe, five by five.", "In transit HQ.", "Initiating.", "It shall be done.",
-    "It will be done.", "It's show time!", "Let's roll.", "Locus acknowledged.", "Make it happen.", "Move it!", "My path is set.", "Naturally.",
-    "Navcom locked.", "No problem.", "No sweat!", "Of course, mein Herr!", "Of course.", "Oh, is that it?", "On my way!", "Orders received.",
-    "Outstanding!", "Perfect!", "Proceedin'", "Right away, sir.", "Right away.", "Rock and roll", "Roger that.", "Roger.", "Set a course.", "Slammin'!",
-    "So be it.", "Stat!", "Sure thing!", "Target designated.", "Thus I serve!", "Vector locked in.", "Very well.", "We move.", "Yeah, I'm goin'!",
-    "Yep!", "You got it.", "You think as I do."
-)
 
 app = App(
     token=configuration['slack']['token'],
@@ -47,7 +35,7 @@ app = App(
     logger=logger,
     process_before_response=configuration['slack']['lambda']
 )
-slack_web_client = WebClient(token=configuration['slack']['token'])
+client = WebClient(token=configuration['slack']['token'])
 
 
 def execute_command(text: str) -> Type[View]:
@@ -74,7 +62,7 @@ def mention(event):
         logger.debug(f"block : {block_response}")
         logger.debug(f"block length : {len(str(block_response))}")
         logger.debug(f"text : {len(text_response)}")
-        slack_web_client.chat_postMessage(
+        client.chat_postMessage(
             channel=event['channel'],
             blocks=block_response,
             text=text_response
@@ -95,7 +83,7 @@ def slash_command(body):
         logger.debug(f"block : {block_response}")
         logger.debug(f"block length : {len(str(block_response))}")
         logger.debug(f"text : {len(text_response)}")
-        slack_web_client.chat_postEphemeral(
+        client.chat_postEphemeral(
             channel=body['channel_id'],
             user=body['user_id'],
             blocks=block_response,
@@ -117,12 +105,3 @@ app.event("app_mention")(ack=acknowledge, lazy=[mention])
 def start():
     app.start(port=int(configuration['slack']['port']))
 
-
-def start_lambda(event, context):
-    logger.debug(f"event : `{event}`")
-    logger.debug(f"context : `{context}`")
-    return SlackRequestHandler(app=app).handle(event, context)
-
-
-if __name__ == '__main__':
-    start()
--- setup.py
+++ setup.py
@@ -1,13 +1,40 @@
 from setuptools import setup, find_packages
 
+import os
+
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 
+def load_requirements():
+    if os.getenv('PYSTONK_RECEIVER_DEPLOY'):
+        # only used for deploying first lambda function to aws (for production)
+        return [
+            "boto3",
+            "pyhocon",
+            "slack_bolt"
+        ]
+    else:
+        #  used for all other situations
+        return [
+            "boto3",
+            "numpy",
+            "prettytable",
+            "pyhocon",
+            "quickchart.io",
+            "requests",
+            "scipy",
+            "slack_bolt",
+            "termcolor",
+            "wheel",
+            "urllib3"
+        ]
+
+
 setup(
     name="pystonk",
-    version="2.1.1",
+    version="2.2.1",
     author="Ethan Lu",
     author_email="fang.lu@gmail.com",
     description="Python Stocks & Options Tool",
@@ -23,19 +50,7 @@ setup(
     python_requires='>=3.10',
     packages=find_packages(),
     package_data={'pystonk': ['conf/*.conf']},
-    install_requires=[
-        "boto3",
-        "numpy",
-        "prettytable",
-        "pyhocon",
-        "quickchart.io",
-        "requests",
-        "scipy",
-        "slack_bolt",
-        "termcolor",
-        "wheel",
-        "urllib3"
-    ],
+    install_requires=load_requirements(),
     extras_require={
         "tests": [
             "coverage",
